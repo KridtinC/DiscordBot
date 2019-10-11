664e94b77e77a6acaa8d99ecbd4a36345ef6fb8c
KNTXTremE <kan.horwang@gmail.com>
2019-10-12 00:10:20 +0700

    Ignore auth.json

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-var auth = require('./auth.json');
+var auth = require('./auth/auth.json');
 const ytdl = require('ytdl-core');
 
 client.on('ready', () => {
